6c97e97c32764e099cd06e3db13ee3b76fbd2653
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2021-08-02 23:38:20 +0530

    fix tsconfig target

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,6 @@
 const DISCORD_EPOCH = 1420070400000
+
+// @ts-expect-error bigint notation
 const DISCORD_EPOCH_BI = 1420070400000n
 
 const MIME_TYPES = {
@@ -14,15 +16,20 @@ export const sleep = (timeout: number) => new Promise(resolve => setTimeout(reso
 export function getTimestampFromSnowflake(snowflake: string) {
   if (!snowflake) return
   const int = BigInt.asUintN(64, BigInt(snowflake))
+  // @ts-expect-error bigint notation
   const dateBits = Number(int >> 22n)
   return new Date(dateBits + DISCORD_EPOCH)
 }
 
+// @ts-expect-error bigint notation
 let increment = 0n
 
+// @ts-expect-error bigint notation
 export function generateSnowflake(timestamp = Date.now(), workerID = 1n, processID = 1n) {
+  // @ts-expect-error bigint notation
   if (increment >= 4095n) increment = 0n
   // timestamp, workerID, processID, increment
+  // @ts-expect-error bigint notation
   return ((BigInt(timestamp) - DISCORD_EPOCH_BI) << 22n) | (workerID << 17n) | (processID << 12n) | increment++
 }
 
