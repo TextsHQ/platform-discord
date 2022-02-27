8d62d67f6967f691a3e22d724fde0697f792428f
Kishan Bagaria <1093313+KishanBagaria@users.noreply.github.com>
2022-02-27 14:42:44 +0530

    remove beta tag

--- src/info.ts
+++ src/info.ts
@@ -3,7 +3,6 @@ import { PlatformInfo, MessageDeletionMode, Attribute } from '@textshq/platform-
 const info: PlatformInfo = {
   name: 'discord',
   version: '2021.08.01',
-  tags: ['Beta'],
   displayName: 'Discord',
   icon: `
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
